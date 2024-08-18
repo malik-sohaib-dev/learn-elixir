c3564ab02477eafd2e8c562746b0a9ede2f7d5d3
Sohaib <asohaib575@gmail.com>
2024-08-18 14:35:21 +0500

    Flow control

---
+++ t7_flow_control/README.md
@@ -0,0 +1,21 @@
+# FlowControl
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `t7_flow_control` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:t7_flow_control, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at <https://hexdocs.pm/t7_flow_control>.
+
---
+++ t7_flow_control/lib/t7_flow_control.ex
@@ -0,0 +1,55 @@
+defmodule FlowControl do
+  @moduledoc """
+  Documentation for `FlowControl`.
+  """
+  def check_max_flying_load(load) do
+    if load < 55, do: "Safe to fly!", else: "Too heavy to fly!"
+    # unless load < 55, do: "Too heavy to fly!", else: "Safe to fly!"
+  end
+
+  def check_fuel_level(percentage) do
+    cond do
+      percentage == 100 ->
+        hehe = 4 * 4
+        haha = :math.pow(hehe, 2)
+        "full tank #{haha}"
+
+      percentage > 75 ->
+        "3/4 tank"
+
+      percentage > 49 ->
+        "1/2 tank"
+
+      percentage > 24 ->
+        "1/4 tank"
+
+      true ->
+        "empty tank"
+    end
+  end
+
+  def error_code_check(value) do
+    case value do
+      200 -> :ok
+      201 -> :created
+      204 -> :no_content
+      n when is_integer(n) and n > 299 -> :error
+      _ -> :unknown
+    end
+  end
+
+  defp convert_lb_to_kg(lb_value), do: lb_value / 2.2
+
+  def equipment_check(equipment_tuple) do
+    case equipment_tuple do
+      {weight, unit_type, quantity} when weight / quantity < 16 and unit_type == :kg ->
+        :equipment_cleared
+
+      {weight, unit_type, quantity} when unit_type == :lb ->
+        if convert_lb_to_kg(weight) / quantity < 16, do: :equipment_cleared, else: :failed
+
+      _ ->
+        :failed
+    end
+  end
+end
